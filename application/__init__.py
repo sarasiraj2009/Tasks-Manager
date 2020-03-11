ee009ca5d9ee7fe2eab1684075ce3b80088917c0
Sara Mohamed <sarasiraj2009@gmail.com>
2020-03-11 21:38:11 +0000

    Processing

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,18 @@
-# import Flask class from the flask module
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from os import getenv
+from flask_login import LoginManager
 
-# create a new instance of Flask and store it in app 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
 
-# import the ./application/routes.py file
+app.config['SQLALCHEMY_DATABASE_URI'] = ('mysql+pymysql://' + getenv('MYSQL_USER') + ':' + getenv('MYSQL_PASS') + '@' + getenv('MYSQL_URL') + '/' + getenv('MYSQL_DB'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'YOUR_SECRET_KEY'
+db = SQLAlchemy(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
 from application import routes
